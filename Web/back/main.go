12abe547877f6e95a5af02f9883158cef965a6e0
Amet <amet.kemal0032@gmail.com>
2025-01-10 07:21:37 +0000

    .exe

--- Web/back/main.go
+++ Web/back/main.go
@@ -18,11 +18,9 @@ func main() {
 	router.HandleFunc("/personal_cabinet", handlers.About)
 	router.HandleFunc("/creating", handlers.TestCreator)
 	router.HandleFunc("/index", handlers.IndexHandler)
-
-	router.HandleFunc("/", handlers.Starter)
-	fileServer := http.FileServer(http.Dir("/public"))
-	router.Handle("/public/", http.StripPrefix("/public/", fileServer))
-
+	// router.HandleFunc("/", handlers.Starter)
+	// fileServer := http.FileServer(http.Dir("/public"))
+	// router.Handle("/public/", http.StripPrefix("/public/", fileServer))
 	// Запускаем сервер и обрабатываем возможные ошибки
 	log.Println("Запуск сервера на порту :5502 ")
 	if err := http.ListenAndServe(":5502", router); err != nil {
